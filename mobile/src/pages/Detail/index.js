1a7428d8f9b5cfb5b601c7e0b67e4bd462cc1c77
alexddsilva <alex.ddsilva@gmail.com>
2020-03-30 22:05:35 -0300

    organização código

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -21,64 +21,65 @@ export default function Detail(){
       }).format(incident.value)}
   `
 
-  function navigateBack(){
+  function navigateGoBack() {
     navigation.goBack();
   }
 
-  function sendMail(){
+  function sendWhatsApp() {
+    Linking.openURL( `whatsapp://send?phone=${ incident.whatsapp }&text=${ message }` );
+  }
+
+  function sendEmail() {
     MailComposer.composeAsync({
-      subject: `Heroi do caso: ${incident.title}`,
-      recipients: [incident.email],
-      body: message
+      subject: `Herói do caso: ${ incident.title }`,
+      recipients: [ incident.email ],
+      body: message,
     })
   }
 
-  function sendWhatsApp(){
-    Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`)
-  }
-
   return (
-    <View style={styles.cointainer}>
-      <View style={styles.header}>
-        <Image source={logoImg} />
+    <View style={ styles.container }>
+      <View style={ styles.header }>
+        <Image source={ logoImg } />
 
-        <TouchableOpacity onPress={navigateBack}>
-          <Feather name="arrow-left" size={28} color="#E02041" />
+        <TouchableOpacity onPress={ navigateGoBack }>
+          <Feather name="arrow-left" size={ 28 } color="#e82041" />
         </TouchableOpacity>
       </View>
-      
-      <View style={styles.incident}>
-          <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-          <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
-          <Text style={styles.incidentProperty}>CASO:</Text>
-          <Text style={styles.incidentValue}>{incident.title}:</Text>
+      <View style={ styles.incident }>
+        <Text style={[ styles.incidentProperty, { marginTop: 0 } ]}>ONG:</Text>
+        <Text style={ styles.incidentValue }>{ incident.name } de { incident.city }/{ incident.uf }</Text>
 
-          <Text style={styles.incidentProperty}>Valor:</Text>
-          <Text style={styles.incidentValue}>{
-            Intl.NumberFormat('pt-BR', { 
-              style: 'currency', 
-              currency: 'BRL'
-              }).format(incident.value)}
-          </Text>
+        <Text style={ styles.incidentProperty }>CASO:</Text>
+        <Text style={ styles.incidentValue }>{ incident.title }</Text>
+
+        <Text style={ styles.incidentProperty }>VALOR:</Text>
+        <Text style={ styles.incidentValue }>
+          { Intl.NumberFormat( 'pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          } ).format( incident.value ) }
+        </Text>
       </View>
 
-      <View style={styles.contactBox}>
-        <Text style={styles.heroTitle}>Salve o dia!</Text>
-        <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+      <View style={ styles.contactBox }>
+        <Text style={ styles.heroTitle }>Salve o dia!</Text>
+        <Text style={ styles.heroTitle }>Seja o herói desse caso.</Text>
 
-        <Text style={styles.heroDescription}>Entre em contato:</Text>
+        <Text style={ styles.heroDescription }>Entre em contato:</Text>
 
-        <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={sendWhatsApp}>
-            <Text style={styles.actionText}>WhatsApp</Text>
+        <View style={ styles.actions }>
+          <TouchableOpacity style={ styles.action } onPress={ sendWhatsApp }>
+            <Text style={ styles.actionText }>WhatsApp</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.action} onPress={sendMail}>
-            <Text style={styles.actionText}>E-mail</Text>
+          <TouchableOpacity style={ styles.action } onPress={ sendEmail }>
+            <Text style={ styles.actionText }>E-mail</Text>
           </TouchableOpacity>
         </View>
       </View>
+
     </View>
   );
 }
